88c1cfa666c6a5c4e92edbb8b67ca0d8b100f8e5
mbagatini <morganabagatini@gmail.com>
2019-11-02 23:45:00 -0300

    Aplicação

--- index.php
+++
@@ -1,5 +0,0 @@
-<h1>Hello world!</h1>
-
-https://github.com/bradtraversy/php_rest_myblog/tree/master/api/post
-
-https://www.youtube.com/watch?v=OEWXbpUMODk
---
+++ resources/login.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+
+<?php include_once('header.php'); ?>
+
+<!-- Title Page-->
+<title>Login</title>
+
+<body>
+<div class="page-wrapper">
+    <div class="page-content--bge5">
+        <div class="container">
+            <div class="login-wrap">
+                <div class="login-content">
+                    <div class="login-logo">
+                        <h2 class="title-1">Eventos</h2>
+                    </div>
+                    <div class="login-form">
+                        <form action="" method="post">
+                            <div class="form-group">
+                                <label>E-mail</label>
+                                <input class="au-input au-input--full" type="email" name="email" placeholder="E-mail">
+                            </div>
+                            <div class="form-group">
+                                <label>Senha</label>
+                                <input class="au-input au-input--full" type="password" name="password"
+                                       placeholder="Senha">
+                            </div>
+                            <button class="au-btn au-btn--block au-btn--green m-b-20" type="submit">Entrar</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+<?php include_once('footer.php'); ?>
+
+</body>
+
+</html>
+<!-- end document-->
